f1935bc625d653c0754865625e37d707606feb60
Hector Quemada <Hector@OSX-HQUEMADA.local>
2020-03-19 17:04:11 -0400

    2020-03-19

---
+++ poem.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+  printf("Dreams: A poem by Langston Hughes\n\n");
+  printf("Hold fast to dreams\n");
+  printf("For if dreams die\n");
+  printf("Life is a broken-winged bird\n");
+  printf("That cannot fly.\n\n");
+  printf("Hold fast to dreams\n");
+  printf("For when dreams go\n");
+  printf("Life is a barren field\n");
+  printf("Frozen with snow.\n");
+  return 0;
+}
